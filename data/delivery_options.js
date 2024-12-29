291fc8dd40e7c2b8aeb0cd3f4e8cc6e77d0162f9
AAKASHEE <aakashpatra253@gmail.com>
2024-12-29 16:09:36 +0530

    save data generate html for delivery options

---
+++ data/delivery_options.js
@@ -0,0 +1,15 @@
+export const deliveryOptions = [{
+    id: '1',
+    delivery_days: 7,
+    priceCents: 0
+},
+{
+    id: '2',
+    delivery_days: 3,
+    priceCents: 499
+    },
+    {
+        id: '3',
+        delivery_days: 1,
+        priceCents: 999
+    }];
