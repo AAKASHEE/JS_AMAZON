1f042af257b847adc2881dbf24630a313e3a8d04
AAKASHEE <aakashpatra253@gmail.com>
2024-12-31 13:57:10 +0530

    testing framwork donw with jasmine and removed default test

---
+++ tests.jasmine/lib/jasmine-5.1.1/moneyTest.js
@@ -0,0 +1,14 @@
+import { formatCurrency } from '../../../scripts/utils/money.js';
+
+describe('test suite:format Currency', () => {
+    it('Converts Cents Into Dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('Works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('works with rounding Off', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
+
