1183974c792e3340162bc9ad9e9eda171e24db4a
AAKASHEE <aakashpatra253@gmail.com>
2025-01-03 00:50:29 +0530

    create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,7 +1,8 @@
 import { cart } from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/delivery_options.js';
-import { formatCurrency }  from '../utils/money.js';
+import { formatCurrency } from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
 
 
@@ -20,8 +21,8 @@ export function renderPaymentSummary() {
     
 
     const totalBeforeTax = productPriceCents + shippingPriceCents;
-    const taxCents =totalBeforeTax * 0.1;
-    const totalCents=totalBeforeTax+taxCents;
+    const taxCents = totalBeforeTax * 0.1;
+    const totalCents = totalBeforeTax + taxCents;
 
     console.log(totalCents);
    
@@ -55,7 +56,7 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
             Place your order
         </button>
     
@@ -63,5 +64,30 @@ export function renderPaymentSummary() {
     `;
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML;
+    document.querySelector('.js-place-order').addEventListener('click', async () => {
+        try {
+            const response=await fetch('https://supersimplebackend.dev/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    
+                    
+                }, body: JSON.stringify({
+                    cart:cart
+                })
+    
+            })
+            const order = await response.json()
+            addOrder(order);
+            
+        } catch (error) {
+            console.log('Unexpected error.Try again later.')
 
+        }
+
+        window.location.href = 'orders.html';
+        
+
+    });
 }
+
